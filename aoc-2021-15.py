ca9e3b59a0e84d99c0ce232127c56cfe317aa349
rky-w <57967688+rky-w@users.noreply.github.com>
2022-02-13 01:26:47 +0000

    Testing with puzldat, may be inefficient

--- aoc-2021-15.py
+++ aoc-2021-15.py
@@ -24,16 +24,17 @@ testdat = [
 def getarr(lst):
     return np.array([list(x) for x in lst], dtype=int)
 
-arr = getarr(testdat)
+arr = getarr(puzldat)
 arr = arr[0:3, 0:3]
 
 start = (0, 0)
 seen = []
 ncs = [(1, 0), (0, 1), (0, -1), (-1, 0)]
 
-def pather(start, seen):
+def pather(start, seen=None):
     print(f"Starting with start={start}")
-
+    if seen == None:
+        seen = []
     if start == (arr.shape[0]-1, arr.shape[1]-1):
         print(f"Terminal reached")
         return arr[start]
@@ -48,8 +49,44 @@ def pather(start, seen):
 
     for nxt in nxts:
         return arr[start] + pather(nxt, deepcopy(seen))
-        print(f"Returning from terminal, cost={cst}, st={arr[start]}, ot={ot}")
 
 
-pather(start, seen)
+pather(start)
+
+
+
+
+costs = []
+def pather(start, seen=None, cost=None):
+    if cost == None:
+        cost = []
+    # print(f"Starting with start={start}, val={arr[start]}, cost={cost}")
+    if seen == None:
+        seen = []
+    if len(costs) > 0 and sum(cost) > min(costs):
+        return    
+    if start == (arr.shape[0]-1, arr.shape[1]-1):
+        costs.append(sum(cost))
+        # print(f"Terminal reached:\n\tPath: {cost}\n\tCost: {sum(cost)}")
+        return
+
+    seen.append(start)
+    chklst = []
+    for nc in ncs:
+        nl = tuple([sum(x) for x in zip(start, nc)])
+        if 0 <= nl[0] <= arr.shape[0]-1 and 0 <= nl[1] <= arr.shape[1]-1 and nl not in seen:
+            chklst.append(nl)
+    # nxts = [x for x in chklst if arr[x] == min([arr[x] for x in chklst])]
+    nxts = chklst
+    
+    arr
+
+    for nxt in nxts:
+        cost.append(arr[nxt])
+        pather(nxt, deepcopy(seen), deepcopy(cost))
+        cost.pop()
+
+pather(start)
+min(costs)
+costs
 
