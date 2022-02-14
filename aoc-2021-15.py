24e3069c1b80faa6c499c4882417ec9fa5bed8bf
rky-w <57967688+rky-w@users.noreply.github.com>
2022-02-14 02:20:10 +0000

    Pt1 solved. Removed recursion implementation which only updated mincost var

--- aoc-2021-15.py
+++ aoc-2021-15.py
@@ -1,4 +1,5 @@
 
+import sys
 from aocd.models import Puzzle
 from copy import deepcopy
 from collections import defaultdict, Counter
@@ -24,11 +25,12 @@ testdat = [
 def getarr(lst):
     return np.array([list(x) for x in lst], dtype=int)
 
+
+# Brute force recursion approach doesn't work, would take too long
 arr = getarr(testdat)
-# arr = arr[:5, :5]
 
 start = (0, 0)
-ncs = [(1, 0), (0, 1)] # , (0, -1), (-1, 0)
+ncs = [(1, 0), (0, 1), (0, -1), (-1, 0)] 
 
 costs = []
 def pather(start, seen=None, cost=None):
@@ -41,7 +43,7 @@ def pather(start, seen=None, cost=None):
         return    
     if start == (arr.shape[0]-1, arr.shape[1]-1):
         costs.append(sum(cost))
-        print(f"Terminal reached:\n\tPath: {cost}\n\tCost: {sum(cost)}")
+        # print(f"Terminal reached:\n\tPath: {cost}\n\tCost: {sum(cost)}")
         return
 
     seen.append(start)
@@ -60,38 +62,98 @@ def pather(start, seen=None, cost=None):
 
 pather(start)
 min(costs)
-arr
 
 
-def pather(start, seen=None, cost=None):
-    global mincost
-    if cost == None:
-        cost = []
-    # print(f"Starting with start={start}, val={arr[start]}, cost={cost}")
-    if seen == None:
-        seen = []    
-    if mincost != None and sum(cost) > mincost:
-        return
-    if start == (arr.shape[0]-1, arr.shape[1]-1):
-        mincost = sum(cost)
-        print(f"Terminal reached:\n\tPath: {cost}\n\tCost: {mincost}")
-        return
 
-    seen.append(start)
-    chklst = []
-    for nc in ncs:
-        nl = tuple([sum(x) for x in zip(start, nc)])
-        if 0 <= nl[0] <= arr.shape[0]-1 and 0 <= nl[1] <= arr.shape[1]-1 and nl not in seen:
-            chklst.append(nl)
-    # nxts = [x for x in chklst if arr[x] == min([arr[x] for x in chklst])]
-    nxts = chklst
+# Initialise cost for first row and column, then find minimum for interior cells
+arr = getarr(puzldat)
+nar = deepcopy(arr)
+nar[0, 0] = 0
 
-    for nxt in nxts:
-        cost.append(arr[nxt])
-        pather(nxt, deepcopy(seen), deepcopy(cost))
-        cost.pop()
+for i in range(1, nar.shape[0]):
+    nar[i, 0] += nar[i-1, 0]
 
-mincost = None
-pather(start)
-mincost
-arr
+for j in range(1, nar.shape[1]):
+    nar[0, j] += nar[0, j-1]
+
+for y in range(1, nar.shape[0]):
+    for x in range(1, nar.shape[1]):
+        nar[x, y] = nar[x, y] + min(nar[x, y-1], nar[x-1, y])
+
+# Incorrect result as paths can only go down and right
+print(nar[nar.shape[0]-1, nar.shape[1]-1]-nar[0,0])
+
+
+
+# Implementation of Dijkstra's algorithm
+def grapher(array):
+    nodes = []
+    coords = []
+    revcoords = []
+    costs = [] 
+    edges = []
+
+    node = 0
+    for x in range(array.shape[0]):
+        for y in range(array.shape[1]):
+            nodes.append(node)
+            coords.append((x, y))
+            costs.append(array[x, y])
+            node += 1
+
+    revcoords = dict(zip(coords, nodes))
+    
+    tsts = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    for node in nodes:
+        _edges = set()
+        for tst in tsts:
+            nc = tuple(map(sum, zip(coords[node], tst)))
+            if nc in revcoords.keys():
+                _edges.add(revcoords[nc])
+        edges.append(_edges)
+
+    Graph = {
+        'nodes': nodes,
+        'coords': coords,
+        'revcoords': revcoords,
+        'costs': costs,
+        'edges': edges,
+        'array': array
+    }
+
+    return Graph
+
+
+def dijkstra(graph):
+
+    Q = set(graph['nodes'])
+    dist = defaultdict(lambda: np.inf)
+    dist[0] = 0
+
+    end = max(graph['nodes'])
+
+    while Q:        
+        newdist = {q: dist[q] for q in Q}
+        u = min(newdist, key=newdist.get)
+        Q.remove(u)
+
+        cn = Q.intersection(graph['edges'][u])
+        if end in cn:
+            return dist[u] + graph['costs'][end]
+
+        for v in Q.intersection(graph['edges'][u]):
+            alt = dist[u] + graph['costs'][v]
+            if alt < dist[v]:
+                dist[v] = alt
+
+    return dist[len(dist)-1]
+
+arr = getarr(testdat)
+
+# Pt 1. Answer
+print(dijkstra(grapher(arr)))
+
+
+
+# Pt 2. Is 5 times larger, may need to speed up the above implementation
+
