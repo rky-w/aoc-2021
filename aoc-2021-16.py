e82002edcb1e96aa61027c4b719d465490a001f0
rky-w <57967688+rky-w@users.noreply.github.com>
2022-02-18 23:39:49 +0000

    Pt 1 completed

--- aoc-2021-16.py
+++ aoc-2021-16.py
@@ -7,7 +7,6 @@ import numpy as np
 import pandas as pd
 import time
 
-
 puzzle = Puzzle(year=2021, day=16)
 puzldat = [val for val in puzzle.input_data.splitlines()]
 
@@ -31,25 +30,65 @@ hexbin = {
 }
 
 
-def h2b(hex):
-    return ''.join([hexbin[h] for h in hex])    
+def h2b(hex) -> list:
+    return [v for h in hex for v in hexbin[h]]
 
-def ver(bin):
-    return int(bin[:3], 2)
+def header(p) -> tuple:
+    v = int(''.join([p.pop(0) for i in range(3)]), 2)
+    t = int(''.join([p.pop(0) for i in range(3)]), 2)
+    return v, t
 
-def typ(bin):
-    return int(bin[3:6], 2)
-
-def litval(bin):
-    lv = bin[7:]
-    vl = ''
+def litval(p) -> int:
+    l = []
     while True:
-        cont = lv[0]
-        vl.join(lv)
-        
-    pass
+        l.extend([p.pop(1) for i in range(4)])
+        if not int(p.pop(0)):
+            break
+    return int(''.join(l), 2)
 
-h2b('D2FE28')
-ver(h2b('D2FE28'))
-typ(h2b('D2FE28'))
+   
+def reader(pk, debug=False):
+    global versum
+    pv, pt = header(pk)
+    versum += pv
+    if debug: print("\n---------------------------------------------------------------------")
+    if debug: print(f"New reader. pv: {pv}, pt: {pt}, versum: {versum}, pk:\n   {''.join(pk)}")
+    if pt == 4:
+        lv = litval(pk)
+        if debug: print(f"Litval hit: {lv}")
+        return
+    else:
+        if int(pk.pop(0)):
+            pkn = int(''.join([pk.pop(0) for i in range(11)]), 2)
+            if debug: print(f"Operator[1] pkn: {pkn}")
+            while pkn:
+                if debug: print(f"op1 loopstart, pk:\n   {''.join(pk)}")
+                reader(pk)
+                pkn -= 1
+                if debug: print(f"op1 loopend, pk:\n   {''.join(pk)}")
+            if debug: print(f"Exiting op1, pk:\n   {''.join(pk)}")
+        else:
+            pkl = int(''.join([pk.pop(0) for i in range(15)]), 2)
+            if debug: print(f"Operator[0] pkl: {pkl}, pk:\n   {''.join(pk)}")
+            while pkl:
+                pklh = len(pk)
+                if debug: print(f"op0 loopstart, pk:\n   {''.join(pk)}")
+                reader(pk)
+                pkln = len(pk)
+                pkl -= pklh - pkln
+                if debug: print(f"op0 loopend, pk:\n   {''.join(pk)}")
+            if debug: print(f"Exiting op0, pk: \n   {''.join(pk)}")
+    return
+
+
+veradd = 0
+for packet in puzldat:
+    versum = 0
+    pk = h2b(packet)
+    reader(pk)
+    veradd += versum
+veradd
+
+print(f"Pt 1. Answer: {veradd}")
+
 
