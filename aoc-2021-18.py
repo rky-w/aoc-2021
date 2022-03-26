467e497b468fdb72dc6e9c2b6b2991f5a5e6707a
rky-w <57967688+rky-w@users.noreply.github.com>
2022-03-26 14:35:58 +0000

    Traversing nested list, finding levels, defining exploder

--- aoc-2021-18.py
+++ aoc-2021-18.py
@@ -1,4 +1,5 @@
 
+from resource import RLIMIT_FSIZE
 import sys
 from aocd.models import Puzzle
 from copy import deepcopy
@@ -15,12 +16,69 @@ puzldat = [ast.literal_eval(val) for val in puzzle.input_data.splitlines()]
 
 
 td = [[1,1],[2,2],[3,3],[4,4]]
+td = [[1,1],[2,2],[3,3],[4,4], [5,5]]
 
-def adder(d):
-    sm = [d[0]]
-    for lst in d[1:]:
-        sm = [sm, lst]
+
+def adder(dd):
+    sm = dd[0]
+    for d in dd[1:]:
+        sm = [sm, d]
     return sm
 
-adder(td)
+dd = adder(td)
 
+
+def exploder(dd, lvl=0, pth=None, orgdd=None):
+    if orgdd == None:
+        orgdd = dd
+    print(f"Entering exploder\n\tdd: {dd}\n\tlvl: {lvl}\n\tpth: {pth}")
+    if pth == None:
+        pth = []
+    if isinstance(dd[0], int) and isinstance(dd[1], int):
+        print(f"Terminal\n\tdd: {dd}\n\tlvl: {lvl}\n\tpth: {pth}")
+        # return
+    lvl += 1
+    if lvl == 4:
+        print(f"Explode:\n\tdd: {dd[0]}\n\tlvl: {lvl}\n\tpth: {pth + [0]}")
+        print(f"Original dd: {orgdd}")
+        return "boom"
+    for id in range(len(dd)):
+        pth.append(id)
+        return exploder(dd[id], lvl, pth, orgdd)
+            
+exploder(dd)
+bdd = dd[0][0][0][0]
+orgdd = dd
+
+def boom(orgdd, bdd):
+    lv = bdd[0]
+    rv = bdd[1]
+    co = str(orgdd)
+    cb = str(bdd)
+
+    coi = co.index(cb)
+    
+    lf = co[:coi]
+    rlf = lf[::-1]
+    boomadd(rlf, lv)
+
+
+    rf = co[coi+len(cb):]
+   
+
+    lf + cb + rf
+    co
+
+def boomadd(s, v):
+    res = re.finditer('\d+', s)
+    try:
+        m = res.__next__()
+        o = s[:m.start()] + str(int(m.group()) + v) + s[m.end():]
+        return o
+    except:
+        print("No matches found")
+        return s
+    
+
+
+    
