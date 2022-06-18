5b6185081a2f616928c3411a37a995ae603dd07a
rky-w <57967688+rky-w@users.noreply.github.com>
2022-06-18 20:36:17 +0100

    Reorienter defined

--- aoc-2021-19.py
+++ aoc-2021-19.py
@@ -18,6 +18,7 @@ puzzle = Puzzle(year=2021, day=19)
 puzldat = [val for val in puzzle.input_data.splitlines()]
 testdat = [line.rstrip() for line in open('testdat.txt')]
 
+
 def loader(dat):
     lst = []
     for d in dat:
@@ -31,22 +32,29 @@ def loader(dat):
     return lst
 
 
+def reorientor(arr):        
+    pairs = ['zx', 'xy', 'yz', 'yx', 'zy', 'xz']
+    flips = [0, 90, 180, 270]
+    spins = [0, 90, 180, 270]
+    newarrs = []
+    for pair in pairs:
+        for flip in flips:
+            for spin in spins:
+                r = R.from_euler(pair, [flip, spin], degrees=True)
+                newarrs.append(r.apply(arr).round())
+    return newarrs
+
+
 pdat = loader(puzldat)
 tdat = loader(testdat)
 
 
-pairs = ['zx', 'xy', 'yz']
-flips = [0, 180]
-spins = [0, 90, 180, 270]
+ntdat = [reorientor(dat) for dat in tdat]
 
-for pair in pairs:
-    for flip in flips:
-        for spin in spins:
-            print(pair, flip, spin)
-            r = R.from_euler(pair, [flip, spin], degrees=True)
-            print(r.as_euler('xyz', degrees=True))
+lst = [str(n[3]) for n in ntdat[0]]
+pprint(lst)
+len(lst)
+len(set(lst))
+pprint(set(lst))
 
-
-
-
-
+[x[0]+x[1] for x in itertools.permutations(['x','y','z'], r = 2)]
