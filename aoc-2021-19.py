9353525e68e7124ad0a784a19d1fa8e16267194d
rky-w <57967688+rky-w@users.noreply.github.com>
2022-10-26 23:32:25 +0100

    Seeming to get all reorientations, bit brute force with rotation though...

--- aoc-2021-19.py
+++ aoc-2021-19.py
@@ -7,7 +7,7 @@ import itertools
 import numpy as np
 import pandas as pd
 import time
-import math
+import math as m
 import re
 import ast
 from pprint import pprint
@@ -17,6 +17,7 @@ from scipy.spatial.transform import Rotation as R
 puzzle = Puzzle(year=2021, day=19)
 puzldat = [val for val in puzzle.input_data.splitlines()]
 testdat = [line.rstrip() for line in open('testdat.txt')]
+testdat2 = [line.rstrip() for line in open('testdat2.txt')]
 
 
 def loader(dat):
@@ -33,34 +34,31 @@ def loader(dat):
 
 
 def reorientor(arr):        
-    pairs = ['zx', 'xy', 'yz', 'yx', 'zy', 'xz']
-    flips = [0, 90, 180, 270]
-    spins = [0, 90, 180, 270]
-    newarrs = []
+    pairs = [x[0]+x[1] for x in itertools.permutations(['x','y','z'], r = 2)]
+    flips = [0, 90, 180, 270, 360]
+    spins = [0, 90, 180, 270, 360]
+    newarrs = [arr]
     for pair in pairs:
         for flip in flips:
             for spin in spins:
                 r = R.from_euler(pair, [flip, spin], degrees=True)
-                newarrs.append(r.apply(arr).round())
+                arr = r.apply(arr).round().astype(int)
+                if not str(arr) in [str(r) for r in newarrs]:
+                    newarrs.append(arr)
     return newarrs
-
 
 pdat = loader(puzldat)
 tdat = loader(testdat)
-
+tdat2 = loader(testdat2)
 
 ntdat = [reorientor(dat) for dat in tdat]
-
-[set(str(x)) for x in ntdat]
-[str(list(x)) for x in ntdat]
 
 for x in ntdat[0]:
     print(x)
 
-lst = [str(n[3]) for n in ntdat[0]]
-pprint(lst)
-len(lst)
-len(set(lst))
-pprint(set(lst))
+[len(x) for x in ntdat]
+# Seems to be working for the first example case
 
-[x[0]+x[1] for x in itertools.permutations(['x','y','z'], r = 2)]
+# Second example - need to work out how to check for overlap...
+scn0 = tdat2[0]
+scnl = [reorientor(dat) for dat in tdat[1]]
